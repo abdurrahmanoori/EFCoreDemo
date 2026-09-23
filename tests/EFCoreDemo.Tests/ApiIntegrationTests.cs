7e88c18a5bf8dcee7e628325e53bcaedf583a92b
Abdurrahman Noori <66649907+abdurrahmanoori@users.noreply.github.com>
2026-09-23 22:00:50 +0430

    Isolate EF provider in API integration tests

--- tests/EFCoreDemo.Tests/ApiIntegrationTests.cs
+++ tests/EFCoreDemo.Tests/ApiIntegrationTests.cs
@@ -3,6 +3,7 @@ using EFCoreDemo.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 namespace EFCoreDemo.Tests;
